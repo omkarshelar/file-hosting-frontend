4550f05ebc0f312597cacfc543c285f1b1c33e4d
Omkar Shelar <omkarshelar24@gmail.com>
2020-06-01 14:34:52 +0530

    Changed link to open in new tab.

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -6,7 +6,7 @@ const Header = () => {
 					File Uploader
 				</a>
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li><a href="https://github.com/omkarshelar/file-hosting-frontend" class="github-logo">
+					<li><a href="https://github.com/omkarshelar/file-hosting-frontend" target="_blank" class="github-logo">
 						<img src="/assets/GitHub-Mark-Light-120px-plus.png" heigth=40px width=40px/>
 					</a></li>
 					<li><a href="#">Documentation</a></li>
