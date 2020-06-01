ba2f94a8b55fb7cbf2663949d89100d0f68d739c
Omkar Shelar <omkarshelar24@gmail.com>
2020-06-01 14:30:55 +0530

    Logo size changes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # File Hosting
 
 Code for the frontend of file hosting application.
-The application is written in typescript and parceJS bundler.
+The application is written in [Typescript](https://www.typescriptlang.org/) and [parcel](https://parceljs.org/) bundler.
+
--- src/components/Header.ts
+++ src/components/Header.ts
@@ -7,7 +7,7 @@ const Header = () => {
 				</a>
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
 					<li><a href="https://github.com/omkarshelar/file-hosting-frontend" class="github-logo">
-						<img src="/assets/GitHub-Mark-Light-120px-plus.png" heigth=56px width=56px/>
+						<img src="/assets/GitHub-Mark-Light-120px-plus.png" heigth=40px width=40px/>
 					</a></li>
 					<li><a href="#">Documentation</a></li>
 				</ul>
